948e23fe7f59601ab3698ff568e94aa335fc8d50
viresh.tripathi@napses.com <viresh.tripathi@napses.com>
2023-12-20 15:42:18 +0530

    add modal and migration of products

---
+++ admin/migrations/20231220100347-create-products.js
@@ -0,0 +1,30 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("products", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        defaultValue: Sequelize.UUIDV4,
+        type: Sequelize.INTEGER,
+      },
+      title: Sequelize.STRING,
+      description: Sequelize.STRING,
+      price: Sequelize.INTEGER,
+      sku: Sequelize.STRING,
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("products");
+  },
+};
